b5bfc40e8ea75a48940ccc59e78c3c73d6f62ad8
Alex Horodishtyanu <horodishtyanu@gmail.com>
2019-04-02 00:15:28 +0300

    Uploade files from Request or Tomcat
    Signed-off-by: horodishtyanu <horodishtyanu@gmail.com>

--- src/main/java/com/mosdev/restapi/controllers/RestApiController.java
+++ src/main/java/com/mosdev/restapi/controllers/RestApiController.java
@@ -3,9 +3,14 @@ package com.mosdev.restapi.controllers;
 import com.mosdev.restapi.domain.Affiche;
 import com.mosdev.restapi.repos.AfficheRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 @RestController
@@ -23,9 +28,26 @@ public class RestApiController {
     }
 
     @GetMapping("/test")
-    public Object getTest(){
-        
-        Affiche affiche = afficheRepo.findById(2);
-        return affiche.getUrl();
+    public void downloadFile(HttpServletResponse response) throws IOException {
+        File file = new File("C:\\Users\\Alexander\\Desktop\\baner_bags.png");
+
+        String fileName = file.getName();
+        Path filePath = file.toPath();
+        String mediaType = Files.probeContentType(filePath);
+
+        response.setContentType(mediaType);
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename="+fileName);
+        response.setContentLength((int)file.length());
+
+        BufferedInputStream inStream = new BufferedInputStream(new FileInputStream(file));
+        BufferedOutputStream outStream = new BufferedOutputStream(response.getOutputStream());
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+        outStream.flush();
+        inStream.close();
     }
 }
