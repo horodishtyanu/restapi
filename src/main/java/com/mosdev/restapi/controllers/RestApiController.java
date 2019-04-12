90eecbba85ee1b026315567891a2cb172079827c
Alex <horodishtyanu@gmail.com>
2019-04-12 19:42:24 +0300

    New Elastic IP. New service Events

--- src/main/java/com/mosdev/restapi/controllers/RestApiController.java
+++ src/main/java/com/mosdev/restapi/controllers/RestApiController.java
@@ -5,15 +5,17 @@ import com.mosdev.restapi.domain.Events;
 import com.mosdev.restapi.repos.AfficheRepo;
 import com.mosdev.restapi.repos.EventsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class RestApiController {
@@ -36,6 +38,15 @@ public class RestApiController {
     }
 
     @GetMapping("/test")
+    public Object test(){
+
+        Map<String, Object> myArr = new HashMap<>();
+        myArr.put("Events", eventsRepo.findAll());
+        myArr.put("Posters", afficheRepo.findAll().get(0));
+        return myArr;
+    }
+
+    @GetMapping("/downFile")
     public void downloadFile(HttpServletResponse response) throws IOException {
         File file = new File("C:\\Users\\Alexander\\Desktop\\baner_bags.png");
 
